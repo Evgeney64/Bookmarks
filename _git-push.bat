eccba1d7ed8a6972c498daa30374f5f940aa1145
Евгений Роев <roev64@mail.ru>
2021-07-05 13:44:06 +0500

    20210705-02

--- _git-push.bat
+++ _git-push.bat
@@ -1,6 +1,6 @@
 git add Bookmarks
-git add _git-push.bat
-git add _git-pull.bat
+rem git add _git-push.bat
+rem git add _git-pull.bat
 
 git commit -m "20210705-01"
 rem ************* git remote add origin https://github.com/Evgeney64/Bookmarks.git
