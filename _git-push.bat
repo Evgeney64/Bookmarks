37ec0275d9407a528f302b45cd9c4a74f76b0d0c
Евгений Роев <roev64@mail.ru>
2021-07-05 09:21:13 +0500

    20210705-01

---
+++ _git-push.bat
@@ -0,0 +1,9 @@
+git add Bookmarks
+git add _git-push.bat
+git add _git-pull.bat
+
+git commit -m "20210705-01"
+rem ************* git remote add origin https://github.com/Evgeney64/Bookmarks.git
+
+git push -u origin master
+pause
